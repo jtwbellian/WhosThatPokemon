830b3af21fede7c33668e3edf9b8b7dc62f497ed
James <j_b387@txstate.edu>
2017-11-12 22:26:43 -0600

    t1
    Initial Test

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  
+//
+//  Created by James Bellian on 11/12/17.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+   std::cout << "Whos that Pokemon??";
+   return 0;
+}
